dc41a28288ce9a46c5d2ea0fa2a955b9c9887ef6
ANDRO <74494494+Tenandrobilgi@users.noreply.github.com>
2023-01-18 04:42:17 +0300

    Added deleting recent user messages

--- Bot/Commands/Moderation/ban.js
+++ Bot/Commands/Moderation/ban.js
@@ -64,23 +64,50 @@ async function InteractionMain(interaction, args) {
 
     var reason;
     var embedReason;
+    var deleteMessagesDuration;
 
-    if (args[1]) {
-        args.shift()
-        reason = args.join(' ')
-        embedReason = `You were banned from ${interaction.guild.name} for: ${reason}`
-    } else {
-        embedReason = `You were banned from ${interaction.guild.name}.`
+    const deleteMessagesSeconds = {
+        "Previous Hour": 3600,
+        "Previous 3 Hours": 10800,
+        "Previous 6 Hours": 21600,
+        "Previous 12 Hours": 43200,
+        "Previous 24 Hours": 86400,
+        "Previous 3 Days": 259200,
+        "Previous 7 Days": 604800
+    }
+
+    const options = interaction.options._hoistedOptions
+
+    for (let i = 0; i < options.length; i++) {
+        let name = options[i]["name"]
+        let value = options[i]["value"]
+
+        if (name == "reason") {
+            reason = value
+            if (reason) {
+                embedReason = `You were banned from ${interaction.guild.name} for: ${reason}`
+            } else {
+                embedReason = `You were banned from ${interaction.guild.name}.`
+            }
+        }
+        if (name == "delete_messages") {
+            let dmsKeys = Object.keys(deleteMessagesSeconds)
+            for (let i = 0; i < dmsKeys.length; i++) {
+                if (value === dmsKeys[i]) {
+                    deleteMessagesDuration = deleteMessagesSeconds[value]
+                }
+            }
+        }
     }
 
     const pmEmbed = new Discord.EmbedBuilder()
         .setTitle(embedReason)
 
     await member.send({ embeds: [pmEmbed] }).catch(err => {
-        console.log(err)
+        //console.log(err)
     })
 
-    member.ban({ reason: reason }).then(() => {
+    member.ban({ deleteMessageSeconds: deleteMessagesDuration, reason: reason }).then(() => {
         const embed = new Discord.EmbedBuilder()
             .setTitle(member.user.username + "#" + member.user.discriminator + " has been banned.")
         interaction.editReply({ embeds: [embed] })
@@ -99,5 +126,15 @@ module.exports = {
         } else {
             MessageMain(message, args)
         }
+    },
+    async getAutoCompleteReturns(interaction) {
+        const focusedValue = interaction.options.getFocused();
+        const options = ["Previous Hour", "Previous 3 Hours", "Previous 6 Hours", "Previous 12 Hours", "Previous 24 Hours", "Previous 3 Days", "Previous 7 Days"]
+
+        const filtered = options.filter(choice => choice.startsWith(focusedValue));
+
+        await interaction.respond(
+            filtered.map(choice => ({ name: choice, value: choice })),
+        );
     }
 }
